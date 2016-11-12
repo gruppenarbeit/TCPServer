2e1a79b54296ca753815c5b41fe0c374f33e985a
aeggli <aeggli@users.noreply.github.com>
2016-11-12 20:35:26 +0100

    MainActivity added functionality
    Neu mit Wifi wieder einschalten, und Sockettext aktualisieren (vom MainThread aus). TCPServer.java kann 1zu1 im exposimeterApp verwendet werden.

--- app/src/main/java/group_project/test001/AttenuatorMainActivity.java
+++ app/src/main/java/group_project/test001/AttenuatorMainActivity.java
@@ -1,24 +1,21 @@
 package group_project.test001;
 
-
-import android.net.Uri;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
-import com.google.android.gms.appindexing.Action;
 import java.lang.reflect.Method;
+
 
 public class AttenuatorMainActivity extends AppCompatActivity {
 
-    //GoogleApiClient client;
-    Button Update_Connection;
     Button sendScan;
     Button SendLive;
     Button StopLive;
@@ -26,73 +23,70 @@ public class AttenuatorMainActivity extends AppCompatActivity {
     Button SendDETV;
     TextView SocketText;
 
+    // These Attributes are also needed in the "real" App
     WifiManager wifi_manager;
     TCPServer Socket;
-    WifiDataBuffer wifiDataBuffer; // Via WifiDataBufffer messages are being passed between Threads to TCPServer to ESP
+    WifiDataBuffer wifiDataBuffer;
+    Boolean WifiWasOn;
 
     // Dequeues an element from wifiDataBuffer and puts it on a TextView
+    // Called by Thread t, see OnCreate
     public void update_Sockettext() { // TODO: Dequeueing elements schould be done by Matthias Thread
-        if(wifiDataBuffer.isDataWaiting_FromESP()){
+        if (wifiDataBuffer.isDataWaiting_FromESP()) {
             byte[] rowData = wifiDataBuffer.deque_FromESP();
             int maxleng = rowData.length;
             String str = "Messages received:\n";
-            if(maxleng > 200) {
+            if (maxleng > 200) {
                 maxleng = 200;
             }
 
             String messageType = "\nWifiPackage.toString: ";
             for (int i = 0; i < maxleng; i++) {
-                messageType += ( rowData[i] & 0xFF)  + "#";
+                messageType += (rowData[i] & 0xFF) + "#";
             }
             messageType += ",\n";
 
             SocketText.setText(str + messageType);
         }
-        else {
-            SocketText.setText("Queue empty");
-        }
     }
 
 
     // For sending Commands to the ESP.
-    private void SendScanTrigger() { // TODO: This schould be done by Matthias Thread
-        byte[] ScanTriggerPack = {82, 68,   49,   54,   83,   67,   65,   78,   0,   8,   1,   5,   0,   80,   50,   50,   50,   50,   50,   50,   50,    50,   50,  50,   50,   50,   50,   50,   80,   69,   78,   68};
+    private void SendScanTrigger() { // Dev ID =
+        byte[] ScanTriggerPack = {82, 68, 49, 54, 83, 67, 65, 78, 0, 0, 0, 125, 0, 80, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 80, 69, 78, 68};
         wifiDataBuffer.enqueue_ToESP(ScanTriggerPack);
     }
 
-    private void SendLiveTrigger () {
-       // byte[] LiveTriggerPack = {82,   68,   49,   54,   84,   73,   77,   69,    0,    8,    1,    5,    0,    0,    7,   65,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,   80,   69,   78,   68};
-        byte[] LiveTriggerPack = {82,   68,   49,   54,   84};
-                wifiDataBuffer.enqueue_ToESP(LiveTriggerPack);
-        byte[] LiveTriggerPack2 = {73,   77,   69,    0,    8,    1,    5,    0,    0,    7,   65,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,   80,   69,   78,   68};
-        wifiDataBuffer.enqueue_ToESP(LiveTriggerPack2);
+    private void SendLiveTrigger() {
+        // byte[] LiveTriggerPack = {82,   68,   49,   54,   84,   73,   77,   69,    0,    0, 0, 125,    0,    0,    7,   65,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,   80,   69,   78,   68};
+        byte[] LiveTriggerPack = {82, 68, 49, 54, 84, 73, 77, 69, 0, 0, 0, 125, 0, 0, 7, 65, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 80, 69, 78, 68};
+        wifiDataBuffer.enqueue_ToESP(LiveTriggerPack);
     }
 
-    private  void SendLiveStop () {
-        byte[] LiveStopPack = {82,   68,   49,   54,   84,   73,   77,   69,    0,    8,    1,    5,    0,    0,    7,   0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,    0,   80,   69,   78,   68};
+    private void SendLiveStop() {
+        byte[] LiveStopPack = {82, 68, 49, 54, 84, 73, 77, 69, 0, 0, 0, 125, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 80, 69, 78, 68};
         wifiDataBuffer.enqueue_ToESP(LiveStopPack);
     }
 
-    private void SendCaliTrigger () {
-        byte[] calitrigger = {82, 68, 49, 54, 67, 65, 76, 68, 0, 8, 1, 5, 0,  0, 0,     0,     0,     0,     0,     0,     0,     0,     0,     0,     0,     0,    0,     0,    80,   69,   78,   68};
+    private void SendCaliTrigger() {
+        byte[] calitrigger = {82, 68, 49, 54, 67, 65, 76, 68,0, 0, 0, 125, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 80, 69, 78, 68};
         wifiDataBuffer.enqueue_ToESP(calitrigger);
     }
 
-    private void SendDETVTrigger () {
-        byte[] DETVTrigger = {82, 68, 49, 54, 68, 69, 84, 86, 0, 8, 1, 5, 0,  4, 0,     2,     0,     20,     0,     60,     0,     100,     0,     0,     0,     0,    80,     0,    80,   69,   78,   68};
+    private void SendDETVTrigger() {
+        byte[] DETVTrigger = {82, 68, 49, 54, 68, 69, 84, 86, 0, 0, 0, 125, 0, 4, 0, 2, 0, 20, 0, 60, 0, 100, 0, 0, 0, 0, 80, 0, 80, 69, 78, 68};
         wifiDataBuffer.enqueue_ToESP(DETVTrigger);
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity","in OnCreate of AttenuatorMainActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Update_Connection = (Button) findViewById(R.id.Update_Connection);
         sendScan = (Button) findViewById(R.id.send_Scan);
         SendLive = (Button) findViewById(R.id.send_live);
         StopLive = (Button) findViewById(R.id.stop_live);
@@ -100,32 +94,17 @@ public class AttenuatorMainActivity extends AppCompatActivity {
         SendDETV = (Button) findViewById(R.id.send_detail);
         SocketText = (TextView) findViewById(R.id.Sockettext);
 
-        wifi_manager = (WifiManager) this.getSystemService(AttenuatorMainActivity.this.WIFI_SERVICE);
-        WifiConfiguration wifi_configuration = null;
-        wifi_manager.setWifiEnabled(false);
-        try
-        {
-            // Source http://stackoverflow.com/questions/13946607/android-how-to-turn-on-hotspot-in-android-programmatically
-            Method method= wifi_manager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
-            method.invoke(wifi_manager, wifi_configuration, true);
-        }
-        catch (Exception e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-
         wifiDataBuffer = new WifiDataBuffer();
         Socket = new TCPServer(wifiDataBuffer); // Initialise TCPServer as well
 
-        // Update should be made by seperate thread
-        Update_Connection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                update_Sockettext(); // What has been received from ESP so far
-            }
-        });
+        wifi_manager = (WifiManager) this.getSystemService(AttenuatorMainActivity.this.WIFI_SERVICE);
+        WifiConfiguration wifi_configuration = null;
+        if(wifi_manager.getWifiState() == 2 || wifi_manager.getWifiState() == 3) {// Enum Constantes for Wifi_enabling and Wifi_enabled
+            WifiWasOn = true;
+            Log.d("MainActivity","Wifi was turned on @ OnCreate");
+        }
+        else {WifiWasOn = false; Log.d("MainActivity","Wifi was turned off @ OnCreate");}
+
 
         sendScan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -134,7 +113,7 @@ public class AttenuatorMainActivity extends AppCompatActivity {
             }
         });
 
-        SendLive.setOnClickListener(new View.OnClickListener(){
+        SendLive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SendLiveTrigger();
@@ -142,13 +121,13 @@ public class AttenuatorMainActivity extends AppCompatActivity {
         });
 
         StopLive.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
+            public void onClick(View v) {
                 SendLiveStop();
             }
         });
 
         SendCali.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
+            public void onClick(View v) {
                 SendCaliTrigger();
             }
         });
@@ -159,19 +138,26 @@ public class AttenuatorMainActivity extends AppCompatActivity {
             }
         });
 
-
-
-// **********************************************************************************
-//
-// Vom Code hier unten hat André keine Ahnung.
-//
-// **********************************************************************************
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    @Override
+    public void onStop() {
+        Log.d("MainActivity","OnStop of MainActivity");
+
+        // turn Hotspot off.
+        try {
+            Method method = wifi_manager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            WifiConfiguration wifi_configuration = null;
+            method.invoke(wifi_manager, wifi_configuration, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // turn Wifi back on.
+        wifi_manager.setWifiEnabled(WifiWasOn);
+
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -197,13 +183,51 @@ public class AttenuatorMainActivity extends AppCompatActivity {
 
     @Override
     public void onStart() {
-        super.onStart();
+        Log.d("MainActivity","OnStart of MainActivity");
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
 
+
+        wifi_manager = (WifiManager) this.getSystemService(AttenuatorMainActivity.this.WIFI_SERVICE);
+        WifiConfiguration wifi_configuration = null;
+        if(wifi_manager.getWifiState() == 2 || wifi_manager.getWifiState() == 3) {// Enum Constantes for Wifi_enabling and Wifi_enabled
+            Log.d("MainActivity","OnStart: Wifi was turned on @ onStart");
+        }
+        else {Log.d("MainActivity", "OnStart: Wifi was turned off @ onStart");}
+
+        wifi_manager.setWifiEnabled(false);
+        try {
+            // Source http://stackoverflow.com/questions/13946607/android-how-to-turn-on-hotspot-in-android-programmatically
+            Method method = wifi_manager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(wifi_manager, wifi_configuration, true);
+            Log.d("MainActivity","turned Hotspot on");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                Log.d("MainActivity", "Starting Thread, that updates SocketText");
+                while (true) {
+                    AttenuatorMainActivity.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            update_Sockettext();
+                            //Log.d("MainActivity", "In While");
+                        }
+                    });
+
+                    // run the sleep-Code NOT in UI-Thread! Will freeze the App.
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-
-    }
 }
