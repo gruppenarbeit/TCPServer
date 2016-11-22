90d1ce82d6c0774d4b3489c99abc9d1968c0370a
ThisIsMarkus <reimarku@student.ethz.ch>
2016-11-22 16:14:22 +0100

    calibration packet filled with csv data, todo: measure

--- app/src/main/java/group_project/test001/TCPServer.java
+++ app/src/main/java/group_project/test001/TCPServer.java
@@ -1,4 +1,4 @@
-package group_project.test001;
+package com.example.matthustahli.radarexposimeter;
 
 import android.util.Log;
 
@@ -19,8 +19,7 @@ import java.net.Socket;
  * Source: http://stackoverflow.com/questions/1212386/concurrent-and-blocking-queue-in-java
  */
 
-public class TCPServer implements TCP_SERVER {
-    public int get_Progress() { return 0;}
+public class TCPServer {
 
     private Thread serverThread = null;
     private WifiDataBuffer wifiDataBuffer;
@@ -31,7 +30,6 @@ public class TCPServer implements TCP_SERVER {
 
 
     public TCPServer(final WifiDataBuffer wifiDataBuffer) throws IllegalStateException {
-        Log.d("TCPServer","Constructor of TCPServer called");
         this.wifiDataBuffer = wifiDataBuffer;
 
         Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {
@@ -42,10 +40,8 @@ public class TCPServer implements TCP_SERVER {
         };
         Thread t = new Thread() {
             public void run() {
-
                 socket = null;
                 try {
-                    Log.d("TCPServer", "Initialising a socket on port 8080, now waiting for ReadyPack from ESP...");
                     serverSocket = new ServerSocket(); // <-- create an unbound socket first
                     serverSocket.setReuseAddress(true);
                     serverSocket.bind(new InetSocketAddress(8080)); // <-- now bind it
@@ -58,6 +54,7 @@ public class TCPServer implements TCP_SERVER {
                 while (!Thread.currentThread().isInterrupted()) { // TODO: Richtige Abbruchbed
                     try {
                         socket = serverSocket.accept();
+
                         outputStream = socket.getOutputStream();
                         inputStream = socket.getInputStream();
                     } catch (IOException e) {
@@ -78,13 +75,7 @@ public class TCPServer implements TCP_SERVER {
 
                             if (wifiDataBuffer.isDataWaiting_ToESP()){ // send Trigger-Pack if one is available
                                 if(socket.getInetAddress().isReachable(100)){ // check if ESP still in reach
-                                    byte[] Triggerpackage2Send = wifiDataBuffer.dequeue_ToESP();
-                                    if (Triggerpackage2Send.length != 32) {
-                                        throw new IllegalArgumentException("All TriggerPacks must have lenght 32!");
-                                    }
-                                    outputStream.write(Triggerpackage2Send); // send Trigger-Pack
-                                    byte[] HeaderofTrigger = {Triggerpackage2Send[4], Triggerpackage2Send[5], Triggerpackage2Send[6], Triggerpackage2Send[7]};
-                                    Log.d("TCPServer","TCPServer did send a TriggerPackage of Type '"+ new String(HeaderofTrigger) +"' to ESP");
+                                    outputStream.write(wifiDataBuffer.dequeue_ToESP()); // send Trigger-Pack
                                 }
                                 else { // ESP not reachable
                                     Log.d("TCPServer","ESP not reachable anymore");
@@ -136,8 +127,7 @@ public class TCPServer implements TCP_SERVER {
                                     throw new IllegalStateException("Packge doesnt end with PEND, but is: " + ContentString);
                                 }
                                 inStreamBuffer.write(content); // now inStreamBuffer contains Header AND content
-                                wifiDataBuffer.enque_FromESP(inStreamBuffer.toByteArray()); // enqueue received Data into WifiDataBuffer;
-                                Log.d("TCPServer","Received a Package of Type " + headerAndDetails);
+                                wifiDataBuffer.enque_FromESP(inStreamBuffer.toByteArray()); // enqueue received Data into WifiDataBuffer
                             }
 
                         } catch (IOException e) {
@@ -170,6 +160,5 @@ public class TCPServer implements TCP_SERVER {
         };
         t.setUncaughtExceptionHandler(h);
         t.start();
-    }  // end Constructor
-
+    }
 }
